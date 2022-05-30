99bc8949ee2b9c822c14c1328a4634367d57f34e
bulabd <bulabd@outlook.com>
2022-05-30 17:56:56 -0400

    Solve exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,10 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+store = Store.create(name: "Burnaby")
+store = Store.create(name: "Richmond")
+store = Store.create(name: "Gastown")
+
+
+puts Store.count
