eb9e9a68e94fef3835595a72206eb496bdae26be
bulabd <bulabd@outlook.com>
2022-05-30 21:58:14 +0000

    Refactor exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,9 +5,9 @@ puts "----------"
 
 # Your code goes below here ...
 
-store = Store.create(name: "Burnaby")
-store = Store.create(name: "Richmond")
-store = Store.create(name: "Gastown")
+Store.create(name: "Burnaby")
+Store.create(name: "Richmond")
+Store.create(name: "Gastown")
 
 
 puts Store.count
