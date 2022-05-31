2b79d6ec2f36246dc2e1a8ad1aff1fdc05e65283
bulabd <bulabd@outlook.com>
2022-05-30 20:01:28 -0400

    Solve exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,11 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+new_store = gets.chomp()
+
+Store.create!(name: new_store, annual_revenue: 100, mens_apparel: nil, womens_apparel: nil)
+
+@store7 = Store.find_by(id: 7)
+
+@store7.employees.create!(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
